881e7754a0a826d72d1e759ec0d4b6f04ddfbb9d
bui63 <rahulkrsingh100297@gmail.com>
2021-10-02 22:04:32 +0530

    reading properties using @Value("${}") annotation

--- src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.microservices.limitsservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,14 @@ import com.in28minutes.microservices.limitsservice.configuration.Configuration;
 @RestController
 public class LimitsController {
 	
-	@Autowired
-	private Configuration configuration;
+	@Value("${limits-service.minimum}")
+	private int minimum;
+	
+	@Value("${limits-service.maximum}")
+	private int maximum;
 	
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
-		return new Limits(configuration.getMinimum(),
-				configuration.getMaximum());
+		return new Limits(minimum, maximum);
 	}
 }
